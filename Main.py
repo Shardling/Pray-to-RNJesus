4a9e9873fd38dcb64baf7bd74b06a79ed0e96298
Shardling <107162461+Shardling@users.noreply.github.com>
2022-06-13 14:54:48 +1200

    Quiz 2 revamp
    Includes choice to quit once all 3 quizzes have been completed, format gives room for more later quizzes if wanted or needed.

--- Main.py
+++ Main.py
@@ -9,6 +9,9 @@ colour_text_4 = "congrats"
 colour_text_5 = "high"
 colour_text_6 = "low"
 colour_text_7 = "number"
+completion_1 = 0
+completion_2 = 0
+completion_3 = 0
 
 def slow_text_time(str, ms):
   for letter in str:
@@ -24,23 +27,9 @@ def slow_text002(str):
 def explain_1():
   slow_text003("Input a number between 1 and 50.\nA random number is generated between these numbers.\nyour guess if incorrect will tell you if it is too high or too low.\nIf you input a number outside the range you will be prompted to input a number inside the range.\n")
 def explain_2():
-  slow_text003("Input 2 numbers and then an operation(+, -, *, /).\nIt will use the operation and make a total number.\nA random number will be pulled in a range of 0 to 100.\nA prompt will tell you  if your guess is too high or too low.\nIn which case you will be able to input all fields again.\n")
+  slow_text003("Input first number, the operation, and finally second number.\nThese should be separated by a comma and a space after the comma. An example may be:\n4, *, 15.\n")
 def explain_3():
   slow_text002("Input a number, range is not given for this one. You must complete this within 31 guesses or you will need to start from the beginning, including a new random number\n")
-def menu():
-  slow_text005("Choose quiz 1, 2, or 3.\nThe larger the number, the higher the dificulty.\n")
-  quiz_choice = input("")
-  if quiz_choice == "1":
-    explain_1()
-    Quiz_1()
-  elif quiz_choice == "2":
-    explain_2()
-    Quiz_2()
-  elif quiz_choice == "3":
-    explain_3()
-    Quiz_3()
-  else:
-    menu()
 def Quiz_1():
   Random = random.randint(0, 50)
   while True:
@@ -51,7 +40,8 @@ def Quiz_1():
       Question_1 = int(Question_1)
       if Question_1 == Random:
         slow_text005({chalk.greenBright("Correct\n")})
-        menu()
+        completion_1 = 1
+        return completion_1
       elif Question_1 >= 51:
         slow_text003("please input a number between 0 and 50\n")
       elif Question_1 >= Random:
@@ -63,15 +53,14 @@ def Quiz_1():
 def Quiz_2():
   Random_2 = random.randint(0, 100)
   while True:
-    slow_text005(f"input {chalk.blue(colour_text_1)} number\n")
-    num_1 = input("")
-    slow_text005(f"input {chalk.blue(colour_text_2)} number\n")
-    num_2 = input("")
+    collect_num = input("")
+    num_split = collect_num.rsplit(", ")
+    num_1 = num_split[0]
+    operation = num_split[1]
+    num_2 = num_split[2]
     try:
       num_1 = float(num_1)
       num_2 = float(num_2)
-      slow_text005(f"input {chalk.magenta(colour_text_3)} (+, -, *, /)\n")
-      operation = input("")
       if operation == "+" or operation == "-" or operation == "*" or operation == "/":
         formula_1 = num_1 + num_2
         formula_2 = num_1 - num_2
@@ -82,7 +71,8 @@ def Quiz_2():
         print(formula_1)
         if formula_1 == Random_2:
           slow_text005(f"{chalk.greenBright(colour_text_4)}\n")
-          menu()
+          completion_2 = 1
+          return completion_2
         elif formula_1 >= Random_2:
           slow_text005(f"too {chalk.red(colour_text_5)}\n")
         else:
@@ -91,7 +81,8 @@ def Quiz_2():
         print(formula_2)
         if formula_2 == Random_2:
           slow_text005(f"{chalk.greenBright(colour_text_4)}\n")
-          menu()
+          completion_2 = 1
+          return completion_2
         elif formula_2 >= Random_2:
           slow_text005(f"too {chalk.red(colour_text_5)}\n")
         else:
@@ -100,7 +91,8 @@ def Quiz_2():
         print(formula_3)
         if formula_3 == Random_2:
           slow_text005(f"{chalk.greenBright(colour_text_4)}\n")
-          menu()
+          completion_2 = 1
+          return completion_2
         elif formula_3 >= Random_2:
           slow_text005(f"too {chalk.red(colour_text_5)}\n")
         else:
@@ -109,18 +101,19 @@ def Quiz_2():
         print(formula_4)
         if formula_4 == Random_2:
           slow_text005(f"{chalk.greenBright(colour_text_4)}\n")
-          menu()
+          completion_2 = 1
+          return completion_2
         elif formula_4 >= Random_2:
           slow_text005(f"too {chalk.red(colour_text_5)}\n")
         else:
           slow_text005(f"too {chalk.red(colour_text_6)}\n")
     except ValueError:
-      Quiz_2()
+      slow_text002("Please input a number\n")
 def Quiz_3():
   while True:
     Exit = input("Would you like to exit to the menu?\n")
     if Exit.lower() == "yes":
-      menu()
+      return
     else:
       Random_3 = random.randint(-1000000000, 1000000000)
       for i in range(31):
@@ -129,8 +122,9 @@ def Quiz_3():
         try:
           Question_2 = float(Question_2)
           if Question_2 == Random_3:
-            slow_text003({chalk.green(colour_text_4)})
-            menu()
+            slow_text003(f"{chalk.green(colour_text_4)}\n")
+            completion_3 = 1
+            return completion_3
           elif Question_2>= Random_3:
             slow_text005(f"too {chalk.red(colour_text_5)}\n")
             print("You have used" ,chalk.blue(i + 1) , "guesses")
@@ -141,4 +135,21 @@ def Quiz_3():
           print("You have used" ,chalk.blue(i + 1) , "guesses")
           slow_text003("")
       Quiz_3()
-menu()
+
+while True:
+  if completion_1 == 1 and completion_2 == 1 and completion_3 == 1:
+    slow_text003("You have completed all 3 quizes. would you like to continue?\n")
+    end_choice = input("")
+    if end_choice == "no":
+      break
+  slow_text003("Choose quiz 1, 2, or 3.\nThe larger the number, the higher the dificulty.\n")
+  quiz_choice = input("")
+  if quiz_choice == "1":
+    explain_1()
+    completion_1 = Quiz_1()
+  elif quiz_choice == "2":
+    explain_2()
+    completion_2 = Quiz_2()
+  elif quiz_choice == "3":
+    explain_3()
+    completion_3 = Quiz_3()
